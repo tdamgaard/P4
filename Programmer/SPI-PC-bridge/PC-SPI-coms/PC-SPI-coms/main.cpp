90afac21d664b249d627da60170f8e1d382633fd
iLyngklip <mathiaslyngklip@gmail.com>
2017-05-05 12:14:56 +0200

    Tilføjet xcode projekt

---
+++ Programmer/SPI-PC-bridge/PC-SPI-coms/PC-SPI-coms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PC-SPI-coms
+//
+//  Created by Mathias Lyngklip Kjeldgaard on 05/05/2017.
+//  Copyright © 2017 Mathias Lyngklip Kjeldgaard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
